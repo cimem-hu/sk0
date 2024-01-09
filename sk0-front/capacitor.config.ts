e24f8d6f90aea18062357d5be1e4c6e6c95885f4
Rolcho <roland@cimem.hu>
2024-01-09 16:29:42 +0100

    sk0 init

---
+++ sk0-front/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'hu.cimem.sk0-front.app',
+  appName: 'sk0-front',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https',
+  },
+};
+
+export default config;
