8f1322f82f01acffe10320eb3954504fda3e87d5
Olasz Tamás <tamas.olasz1@gmail.com>
2024-01-26 20:02:05 +0000

    Login page generated

--- sk0-front/src/app/app.routes.ts
+++ sk0-front/src/app/app.routes.ts
@@ -9,5 +9,9 @@ export const routes: Routes = [
   {
     path: 'home',
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+  },  {
+    path: 'login',
+    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
   },
+
 ];
