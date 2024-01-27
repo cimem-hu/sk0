4356a9498a2c014889de25d3b2d03b08a8e4a1a6
Olasz Tamás <tamas.olasz1@gmail.com>
2024-01-27 05:43:35 +0000

    Added validation for inputs

--- sk0-front/src/app/login/login.page.ts
+++ sk0-front/src/app/login/login.page.ts
@@ -8,28 +8,28 @@ import {
   Validators,
 } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [
-    IonicModule,
-    CommonModule,
-    FormsModule,
-    MatInputModule,
-    ReactiveFormsModule,
-  ],
+  imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule],
 })
 export class LoginPage {
   loginForm = new FormGroup({
-    email: new FormControl(),
-    password: new FormControl(),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.pattern(
+        /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/ //at least one cap letter, one number, one spec char
+      ),
+      Validators.minLength(6),
+    ]),
   });
   constructor() {}
 
-  loginSubmit() {}
+  loginSubmit() {
+    // TODO: create service file where HTTP req is made, inject service to this comp
+  }
 }
