f5a61e4b9cab06b57f70dd8c36934a10c9d159d7
Olasz Tamás <113930714+Thomas851204@users.noreply.github.com>
2024-01-30 18:08:10 +0100

    Update sk0-front/src/app/app.routes.ts
    Co-authored-by: Roland Nagy <114018162+rolcho@users.noreply.github.com>

--- sk0-front/src/app/app.routes.ts
+++ sk0-front/src/app/app.routes.ts
@@ -12,7 +12,7 @@ export const routes: Routes = [
   },
   {
     path: 'login',
-    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
+    loadComponent: () => import('./login/login.page').then((m) => m.LoginPage)
   },
 
 ];
