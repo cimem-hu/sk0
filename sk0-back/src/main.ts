3964b2f72f8052f3814f8be7ef54ca95df6787e9
Fenyo7 <fenyo.lev@gmail.com>
2024-02-07 18:47:29 +0000

    I hate docker

--- sk0-back/src/main.ts
+++ sk0-back/src/main.ts
@@ -1,17 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
-import * as fs from 'fs';
 
-async function bootstrap() {
+async function bootstrap() { 
+  /*
   const env = process.env.NODE_ENV || 'development';
   const envConfig = dotenv.parse(fs.readFileSync(`.env.${env}`));
   for (const k in envConfig) {
     process.env[k] = envConfig[k];
-  }
+  } */
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT;
-  await app.listen(port, '127.0.0.1');
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  const port = parseInt(process.env.PORT, 10) || 3000;
+  await app.listen(port);
+  console.log(`Application is running on: ${port}`);
 }
 bootstrap();
