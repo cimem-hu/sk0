20e03b26ece1075ac8b1c315b05c7d59a3a44e26
Fenyo7 <fenyo.lev@gmail.com>
2024-02-10 00:56:31 +0000

    added auth module for generating and validating tokens

--- sk0-back/src/app.module.ts
+++ sk0-back/src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 import configuration from './config/configuration';
 
 @Module({
-  imports: [DatabaseModule, UsersModule, ConfigModule.forRoot({load: [configuration]})],
+  imports: [DatabaseModule, UsersModule, ConfigModule.forRoot({load: [configuration]}), AuthModule],
   controllers: [AppController],
   providers: [],
 })
---
+++ sk0-back/src/auth/jwt.strategy.ts
@@ -0,0 +1,25 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { JwtPayload } from './jwt-payload.interface';
+import { UsersService } from 'src/users/services/users.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  userService: UsersService;
+  constructor(
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<any> {
+    const user = await this.userService.findOneByEmail(payload.email);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}
