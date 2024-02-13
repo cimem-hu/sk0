d209e81af446cc10aebd0db45969c5b7d706e59f
bendesbeus <bendesbeatrix@gmail.com>
2024-02-13 13:57:35 +0000

    correcting tests

--- sk0-front/src/app/auth/register/register.page.spec.ts
+++ sk0-front/src/app/auth/register/register.page.spec.ts
@@ -1,12 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RegisterPage } from './register.page';
+import { HttpClient } from '@angular/common/http';
+
+class HttpClientMock extends HttpClient {
+
+}
 
 describe('RegisterPage', () => {
   let component: RegisterPage;
   let fixture: ComponentFixture<RegisterPage>;
-
-  beforeEach(async () => {
-    fixture = TestBed.createComponent(RegisterPage);
+  beforeEach(() => {
+    fixture = TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: HttpClient,
+          useValue: HttpClientMock,
+        },
+      ],
+    })
+      .createComponent(RegisterPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- sk0-front/src/app/home/home.page.spec.ts
+++ sk0-front/src/app/home/home.page.spec.ts
@@ -1,12 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HomePage } from './home.page';
+import { HttpClient } from '@angular/common/http';
+
+class HttpClientMock extends HttpClient {
+
+}
 
 describe('HomePage', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
 
-  beforeEach(async () => {
-    fixture = TestBed.createComponent(HomePage);
+  beforeEach(() => {
+    fixture = TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: HttpClient,
+          useValue: HttpClientMock,
+        },
+      ],
+    })
+      .createComponent(HomePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
