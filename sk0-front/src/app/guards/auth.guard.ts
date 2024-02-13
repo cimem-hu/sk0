ba3df9d03dab54b47d945e9e2fbe2ff28f6934d3
Olasz Tamás <tamas.olasz1@gmail.com>
2024-02-13 17:33:03 +0000

    modified guard so it evaluates behavioursubject value

--- sk0-front/src/app/guards/auth.guard.ts
+++ sk0-front/src/app/guards/auth.guard.ts
@@ -4,8 +4,8 @@ import { inject } from '@angular/core';
 
 export const authGuard: CanActivateFn = () => {
   const router = inject(Router);
-  const authService = inject(AuthService);
-  if (authService.getIsUserLoggedIn()) {
+  const userStatus = inject(AuthService).isUserLoggedIn;
+  if (userStatus.value) {
     return true;
   }
   return router.navigate(['login']);
