58a72e94105b7a458e073d2fe745e8ebddbab135
Janka Palossy <palossy@gmail.com>
2024-02-16 15:14:59 +0000

    logut button added to menu. menu includes SK0-33 (username)

--- sk0-front/src/app/app.component.ts
+++ sk0-front/src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { AuthService } from './auth/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule],
 })
 export class AppComponent {
-  constructor() {}
+  userName$ = this.authService.userName;
+
+  constructor(private authService: AuthService) {}
+
+  logout() {
+    this.authService.logout();
+  }
 }
