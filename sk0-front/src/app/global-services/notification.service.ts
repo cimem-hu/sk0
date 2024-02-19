c61c539aebfccbb01d2f3785ed4fe9aba51dc3ec
Olasz Tamás <tamas.olasz1@gmail.com>
2024-02-19 09:58:58 +0000

    Created methods in notification service for successful login/register messages.

--- sk0-front/src/app/global-services/notification.service.ts
+++ sk0-front/src/app/global-services/notification.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root',
 })
 export class NotificationService {
-  constructor() {}
+  constructor(private toastController: ToastController) {}
+
+  async showSuccessfulLogin() {
+    const toast = await this.toastController.create({
+      message: 'Sikeres belépés',
+      duration: 1500,
+      position: 'top',
+    });
+    await toast.present();
+  }
+  async showSuccessfulRegistration() {
+    const toast = await this.toastController.create({
+      message: 'Sikeres regisztráció',
+      duration: 1500,
+      position: 'top',
+    });
+    await toast.present();
+  }
 }
