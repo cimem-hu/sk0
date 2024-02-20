1a735b4745b400ae0273b2611a42d0c935731de1
Olasz Tamás <tamas.olasz1@gmail.com>
2024-02-20 16:52:41 +0000

    Created basic test case.

--- sk0-front/src/app/global-services/notification.service.spec.ts
+++ sk0-front/src/app/global-services/notification.service.spec.ts
@@ -1,16 +1,26 @@
 import { TestBed } from '@angular/core/testing';
 
 import { NotificationService } from './notification.service';
+import { expect, jest } from '@jest/globals';
 
 describe('NotificationService', () => {
-  let service: NotificationService;
+  let notificationService: NotificationService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(NotificationService);
+    notificationService = TestBed.inject(NotificationService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(notificationService).toBeTruthy();
+  });
+
+  it('should be called with specified message and position', () => {
+    const message = 'Toast message';
+    const position = 'top';
+    const toastSpy = jest.spyOn(notificationService, 'toastMessage');
+    notificationService.toastMessage(message, position);
+    expect(toastSpy).toBeCalled();
+    expect(toastSpy).toBeCalledWith(message, position);
   });
 });
