6a6f5dfea607f24d905e3778aa087c3c1190faac
rolcho <roland@cimem.hu>
2024-02-22 13:51:31 +0000

    updated test

--- sk0-front/src/app/profile/profile.page.spec.ts
+++ sk0-front/src/app/profile/profile.page.spec.ts
@@ -1,12 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ProfilePage } from './profile.page';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { AuthService } from '../auth/auth.service';
 
 describe('ProfilePage', () => {
   let component: ProfilePage;
   let fixture: ComponentFixture<ProfilePage>;
 
   beforeEach(async () => {
-    fixture = TestBed.createComponent(ProfilePage);
+    fixture = TestBed.configureTestingModule({
+      providers: [AuthService],
+      imports: [HttpClientModule],
+    }).createComponent(ProfilePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
