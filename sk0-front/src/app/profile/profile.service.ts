60a8bd117edfd63fc53f1804d3031b4a19ca0222
Mark Kovari <kovarimarkofficial@gmail.com>
2024-03-16 22:16:31 +0100

    fix: add conditional store related profile updateeffect

--- sk0-front/src/app/profile/profile.service.ts
+++ sk0-front/src/app/profile/profile.service.ts
@@ -1,16 +1,16 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { environment } from "src/environments/environment";
-import { ProfileUpdateRequest } from "./store/profile.actions";
+import { ProfileUpdateRequest, ProfileUpdateResponse } from "./store/profile.actions";
 
 @Injectable({
   providedIn: "root"
 })
 export class ProfileService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   update(id: number, profileUpdateDto: ProfileUpdateRequest) {
-    return this.http.patch(
+    return this.http.patch<ProfileUpdateResponse>(
       `${environment.baseUrl}/users/${id}`,
       profileUpdateDto
     );
