073c417a8f4fd336248ea3a0ff167020ba94ba13
Mark Kovari <kovarimarkofficial@gmail.com>
2024-03-16 22:16:31 +0100

    fix: add conditional store related profile updateeffect

--- sk0-front/src/app/profile/store/profile.effects.ts
+++ sk0-front/src/app/profile/store/profile.effects.ts
@@ -1,37 +1,40 @@
 import { Injectable } from "@angular/core";
 import { NavController } from "@ionic/angular";
 import { Actions, concatLatestFrom, createEffect, ofType } from "@ngrx/effects";
-import { catchError, map, mergeMap, of, tap, withLatestFrom } from "rxjs";
+import { catchError, map, mergeMap, of, pipe, switchMap, tap, withLatestFrom } from "rxjs";
 
 import { ProfileService } from "../profile.service";
-import { profileUpdateStarted } from "./profile.actions";
+import { profileUpdateFailure, profileUpdateStarted, profileUpdateSuccess } from "./profile.actions";
 import { Store } from "@ngrx/store";
 import { AppStore } from "src/app/app.store";
 import { getUserId } from "src/app/auth/store/auth.selectors";
+import { Action } from "rxjs/internal/scheduler/Action";
 
 @Injectable()
 export class AuthEffects {
   constructor(
     private readonly actions$: Actions,
     private readonly profileService: ProfileService,
-    private readonly store: Store<AppStore>
+    private readonly store$: Store<AppStore>
 
     // private readonly navCtl: NavController
-  ) {}
+  ) { }
 
   handleUpdateProfileEffects$ = createEffect(() =>
     this.actions$.pipe(
       ofType(profileUpdateStarted),
-      concatLatestFrom((action) => this.store.select(getUserId)),
-      tap([a, b] => {})
-      // tap(([action, id]) =>
-      //   mergeMap((action) =>
-      //     this.profileService.update(id, action).pipe(
-      //       map((response) => loginSuccess(response)),
-      //       catchError((error) => of(loginFailure({ message: error.message })))
-      //     )
-      //   )
-      // )
+      concatLatestFrom(() => this.store$.select(getUserId)),
+      switchMap(([action, id]) => {
+        if (id !== null && id !== undefined) {
+          return this.profileService.update(id, action)
+            .pipe(
+              map((response) => profileUpdateSuccess(response)),
+              catchError((error) => of(profileUpdateFailure({ message: error.message })))
+            );
+        } else {
+          return of(profileUpdateFailure({ message: 'Invalid user id' }));
+        }
+      })
     )
   );
 }
