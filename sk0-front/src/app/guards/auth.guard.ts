c3f79e991c2f7d77542282c98f4f7021918d62ed
rolcho <roland@cimem.hu>
2024-03-17 21:18:18 +0100

    refactor(fe): update to store

--- sk0-front/src/app/guards/auth.guard.ts
+++ sk0-front/src/app/guards/auth.guard.ts
@@ -1,12 +1,17 @@
-import { CanActivateFn, Router } from "@angular/router";
-import { AuthService } from "../auth/auth.service";
+import { CanActivateFn } from "@angular/router";
 import { inject } from "@angular/core";
+import { Store } from "@ngrx/store";
+import { lastValueFrom } from "rxjs";
 
-export const authGuard: CanActivateFn = () => {
-  const router = inject(Router);
-  const userStatus = inject(AuthService).isUserLoggedIn;
-  if (userStatus.value) {
-    return true;
+import { AppStore } from "../app.store";
+import { isUserLoggedIn } from "../auth/store/auth.selectors";
+import { navigateBackToHome } from "../common/store/navigation.actions";
+
+export const authGuard: CanActivateFn = async () => {
+  const store = inject(Store<AppStore>);
+  const userStatus = await lastValueFrom(store.select(isUserLoggedIn));
+  if (!userStatus) {
+    store.dispatch(navigateBackToHome());
   }
-  return router.navigate(["login"]);
+  return userStatus;
 };
