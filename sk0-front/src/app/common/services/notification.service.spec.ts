cd4d666f11a73d20d872f51241fab09edd010a96
rolcho <roland@cimem.hu>
2024-03-18 10:25:44 +0100

    test(fe): services

--- sk0-front/src/app/common/services/notification.service.spec.ts
+++ sk0-front/src/app/common/services/notification.service.spec.ts
@@ -1,42 +1,73 @@
-import { TestBed } from "@angular/core/testing";
 import { expect, jest } from "@jest/globals";
 
 import { NotificationService } from "./notification.service";
+import { AlertController, ToastController } from "@ionic/angular";
 
 describe("NotificationService", () => {
   let notificationService: NotificationService;
+  let toastController: jest.Mocked<ToastController>;
+  let alertController: jest.Mocked<AlertController>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    notificationService = TestBed.inject(NotificationService);
+    toastController = {
+      create: jest.fn().mockResolvedValue({
+        present: jest.fn().mockResolvedValue(null as never)
+      } as never)
+    } as unknown as jest.Mocked<ToastController>;
+
+    alertController = {
+      create: jest.fn().mockResolvedValue({
+        present: jest.fn().mockResolvedValue(null as never)
+      } as never)
+    } as unknown as jest.Mocked<AlertController>;
+
+    notificationService = new NotificationService(
+      toastController,
+      alertController
+    );
   });
 
   it("should be created", () => {
     expect(notificationService).toBeTruthy();
   });
 
-  it("should be called with specified message and position", () => {
+  it("should be called with specified message and position", async () => {
     const message = "Toast message";
     const position = "top";
-    const toastSpy = jest.spyOn(notificationService, "toastMessage");
-    notificationService.toastMessage(message, position);
-    expect(toastSpy).toBeCalled();
-    expect(toastSpy).toBeCalledWith(message, position);
+
+    await notificationService.toastMessage(message, position);
+
+    expect(toastController.create).toHaveBeenCalledWith({
+      color: "primary",
+      message,
+      duration: 1500,
+      position
+    });
+    expect((await toastController.create()).present).toHaveBeenCalled();
   });
 
   it("should call alertSuccess with specified message", async () => {
-    const message = "Success message";
-    const alertSuccessSpy = jest.spyOn(notificationService, "alertSuccess");
-    notificationService.alertSuccess(message);
-    expect(alertSuccessSpy).toHaveBeenCalled();
-    expect(alertSuccessSpy).toHaveBeenCalledWith(message);
+    const message = "Alert success";
+
+    await notificationService.alertSuccess(message);
+
+    expect(alertController.create).toHaveBeenCalledWith({
+      message,
+      buttons: ["OK"]
+    });
+    expect((await alertController.create()).present).toHaveBeenCalled();
   });
 
   it("should call alertError with specified message", async () => {
-    const message = "Error message";
-    const alertErrorSpy = jest.spyOn(notificationService, "alertError");
-    notificationService.alertError(message);
-    expect(alertErrorSpy).toHaveBeenCalled();
-    expect(alertErrorSpy).toHaveBeenCalledWith(message);
+    const message = "Alert success";
+
+    await notificationService.alertError(message);
+
+    expect(alertController.create).toHaveBeenCalledWith({
+      header: "Hiba",
+      message,
+      buttons: ["OK"]
+    });
+    expect((await alertController.create()).present).toHaveBeenCalled();
   });
 });
